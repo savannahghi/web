9a531ef4cc8bf8fae2f6ba7201b9fce75f7ac53c
Abdi Adan <abdi.adan@healthcloud.co.ke>
2021-03-08 16:24:58 +0300

    feat: initial commit

---
+++ components/navbar.js
@@ -0,0 +1,12 @@
+import NavBarStyles from "../styles/modules/navbar.module.css";
+
+export default function NavBar() {
+    return (
+      <div className={NavBarStyles["nav"]}>
+        <div className="">
+          <p className="">navbar</p>
+        </div>
+      </div>
+    );
+  }
+  
---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+
+/**
+ * Next.js uses this component to initialize pages.
+ */
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp

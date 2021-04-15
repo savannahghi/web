ca0374f760321324c42146991ab0be788bffa56a
Abdi Adan <abdi.adan@healthcloud.co.ke>
2021-04-15 17:08:10 +0300

    style: team page redesign

--- components/TeamCard.js
+++ components/TeamCard.js
@@ -1,19 +1,46 @@
 import Image from 'next/image';
+import TeamStyles from '../styles/modules/Team.module.scss';
 
 export default function TeamCard({ props }) {
     return (
         <div className="col-lg-4 col-md-4 col-sm-12">
-            <div className="card sghi-card p-3">
-                <Image
-                    src={props.headshot}
-                    alt="Team Member Photo"
-                    className="rounded-circle"
-                    width={200}
-                    height={200}
-                />
-                <div className="card-body">
-                    <p className="team-name">{props.name}</p>
-                    <p className="team-title">{props.title}</p>
+            <div className="card sghi-card">
+                <div className={TeamStyles.banner}></div>
+                <div className="p-3">
+                    <Image
+                        src={props.headshot}
+                        alt="Team Member Photo"
+                        className="rounded-circle"
+                        width={200}
+                        height={200}
+                    />
+                    <div className="card-body">
+                        <p className="team-name">{props.name}</p>
+                        <p className="team-title">{props.title}</p>
+                        <div>
+                            <a
+                                className="btn btn-primary linkedIn btn-floating mx-2"
+                                href="/"
+                                role="button">
+                                <i className="fab fa-linkedin-in"></i>
+                            </a>
+                            <a
+                                className="btn btn-primary google btn-floating mx-2"
+                                href="/"
+                                role="button">
+                                <i className="fab fa-google"></i>
+                            </a>
+                            <a
+                                className="btn btn-primary twitter btn-floating mx-2"
+                                href="/"
+                                role="button">
+                                <i className="fab fa-twitter"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="btn btn-secondary btn-rounded text-capitalize px-5">
+                        Learn More
+                    </div>
                 </div>
             </div>
         </div>
--- pages/team.js
+++ pages/team.js
@@ -3,6 +3,8 @@ import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
 import TeamCard from '../components/TeamCard';
 import * as StringConstants from '../components/constants/app_strings';
+import TeamData from '../lib/team.json';
+import TeamStyles from '../styles/modules/Team.module.scss';
 
 /**
  * This component is the Teams page
@@ -33,9 +35,34 @@ export default function Team() {
                             </p>
                         </div>
 
-                        {teamData.map((member) => (
-                            <TeamCard key={member.id} props={member} />
-                        ))}
+                        {TeamData.map((member) =>
+                            member.fellow ? null : <TeamCard key={member.id} props={member} />
+                        )}
+                    </div>
+
+                    <div className="row py-5 px-5">
+                        <div className="col-12 my-3">
+                            <p className="page-title text-center">
+                                <span className="page-title-second">Fellows</span> & Advisors
+                            </p>
+                        </div>
+
+                        {TeamData.map((member) =>
+                            member.fellow ? <TeamCard key={member.id} props={member} /> : null
+                        )}
+                    </div>
+                </div>
+
+                <div className="container-fluid bg-light text-center pt-3 pb-5">
+                    <div className={TeamStyles.join_us_title}>Interested in joining the team?</div>
+                    <div className={TeamStyles.join_sub}>
+                        <div className={TeamStyles.join_us_subtitle}>
+                            One of our most important core values is to partner with our employees
+                            because we know they are instrumental in the success of the company.
+                        </div>
+                    </div>
+                    <div className="btn btn-secondary btn-rounded text-capitalize px-5 font-weight-bold">
+                        See Job Openings
                     </div>
                 </div>
             </main>
@@ -44,36 +71,3 @@ export default function Team() {
         </div>
     );
 }
-
-const teamData = [
-    {
-        id: 0,
-        name: 'Dr. Riro Matiko',
-        title: 'SGHI President',
-        headshot: '/images/matiko.svg'
-    },
-    {
-        id: 1,
-        name: 'Dr. John Muthee',
-        title: 'CEO, Savannah Informatics',
-        headshot: '/images/john_muthee.svg'
-    },
-    {
-        id: 2,
-        name: 'Eric Mbuthia',
-        title: 'SGHI Team',
-        headshot: '/images/eric_mbuthia.svg'
-    },
-    {
-        id: 3,
-        name: 'Dr. Ngure Nyaga',
-        title: 'SGHI Team',
-        headshot: '/images/team_member.jpg'
-    },
-    {
-        id: 4,
-        name: 'John Paul Ogalo',
-        title: 'SGHI Team',
-        headshot: '/images/jp_ogalo.svg'
-    }
-];
